14e6fff16fe306377afa5e81a8304a5965ac405b
Tociva Technologies <prince.francis@tociva.com>
2026-01-30 09:20:53 +0530

    theme menu added in playground.

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { categories } from './home/home.component';
+import { TailngMenuComponent, TailngMenuItemDirective } from '@tociva/tailng-ui';
+import { ThemeService } from './shared/theme.service';
 
 @Component({
   selector: 'playground-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, TailngMenuComponent, 
+    TailngMenuItemDirective],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+
+  private readonly themeService = inject(ThemeService);
   year = new Date().getFullYear();
   categories = categories;
+
+  changeTheme(theme: 'light' | 'dark'): void {
+    console.log('changeTheme', theme);
+    this.themeService.set(theme);
+  }
 }
---
+++ apps/playground/src/app/shared/theme.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, effect, signal } from '@angular/core';
+
+export type TailngTheme = 'light' | 'dark';
+
+@Injectable({ providedIn: 'root' })
+export class ThemeService {
+  readonly theme = signal<TailngTheme>('dark');
+
+  constructor() {
+    effect(() => {
+      const t = this.theme();
+
+      const el = document.documentElement; // <html>
+      el.classList.remove('theme-light', 'theme-dark');
+      el.classList.add(`theme-${t}`);
+    });
+  }
+
+  set(theme: TailngTheme) {
+    this.theme.set(theme);
+  }
+}
